1bd85882d9e18239ba5207e05803226925641cee
sis-shen <adongfa@gmail.com>
2025-10-22 17:31:38 +0800

    1

--- ConcurrentPattern/main.go
+++ ConcurrentPattern/main.go
@@ -61,10 +61,78 @@ func perchase(ctx context.Context, inCh chan int, outCh chan int) {
 		case <-ctx.Done():
 			fmt.Printf("perchase, done\n")
 			return
-		default:
-
+		case num := <-inCh:
+			fmt.Printf("inCh: 采购了%d\n", num)
+			outCh <- num
 		}
 	}
+}
+
+func machine(ctx context.Context, inCh chan int, outCh chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("machine, done\n")
+			return
+		case num := <-inCh:
+			fmt.Printf("machine:收到加工任务%d\n", num)
+			for i := 0; i < num; i++ {
+				fmt.Printf("machine:正在加工一个产品，编号%d\n", i)
+				outCh <- 1
+				time.Sleep(1 * time.Second)
+			}
+			fmt.Printf("inCh: 生产了%d\n", num)
+		}
+	}
+}
+
+func sell(ctx context.Context, inCh chan int, outCh chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("sell, done\n")
+			return
+		case num := <-inCh:
+			fmt.Printf("sell:获取到销售任务%d\n", num)
+			for i := 0; i < num; i++ {
+				fmt.Printf("sell:卖出去了一个产品，编号%d\n", i)
+				time.Sleep(1 * time.Second)
+			}
+			outCh <- num
+			fmt.Printf("本批次任务完成，共计产品%d\n", num)
+		}
+
+	}
+}
+
+func test_pipeline() {
+	inPerchaseCh := make(chan int)
+	outPerchaseCh := make(chan int)
+	outMachineCh := make(chan int)
+	outSellCh := make(chan int)
+	perchaseCtx, perchaseCancel := context.WithCancel(context.Background())
+	machineCtx, machineCancel := context.WithCancel(context.Background())
+	sellCtx, sellCancel := context.WithCancel(context.Background())
+
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		perchase(perchaseCtx, inPerchaseCh, outPerchaseCh)
+	}()
+
+	go func() {
+		defer wg.Done()
+		machine(machineCtx, outPerchaseCh, outMachineCh)
+	}()
+
+	go func() {
+		defer wg.Done()
+		sell(sellCtx, outMachineCh, outSellCh)
+	}()
+
+	defer wg.Wait()
 }
 
 func main() {
